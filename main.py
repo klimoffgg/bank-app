ffb8c799089a682a8d6a742ce9b062e3065d8eec
klimoffgg <klimoffit@mail.ru>
2026-03-19 18:39:09 +0300

    Создал функцию создания транзакции

--- main.py
+++ main.py
@@ -1,10 +1,40 @@
-from fastapi import FastAPI, status, HTTPEcxeption
-from pydantic import Basemodel
+from enum import Enum
+from http.client import responses
 
-class TransactionStatus (Basemodel):
+from fastapi import FastAPI, status, HTTPException
+from pydantic import BaseModel
+from enum import Enum
+
+trans = []
+class TransactionStatus (str, Enum):
     DRAFT = 'DRAFT'
     PROCESSING = 'PROCESSING'
     FROZEN = 'FROZEN'
     ARCHIVE = 'ARCHIVE'
+class TransactionRequest(BaseModel):
+    sender: str
+    receiver: str
+    amount: int
+class TransactionResponse (BaseModel):
+    sender: str
+    receiver: str
+    amount: int
+    status: TransactionStatus = TransactionStatus.DRAFT
+    id: int
 
-class TransactionCreate (Basemodel):
+app = FastAPI()
+@app.post(
+    '/transaction',
+    response_model = TransactionResponse,
+    status_code = status.HTTP_201_CREATED,
+)
+def create_transaction(info:TransactionRequest):
+    id = len(trans) + 1
+    transaction = TransactionResponse(
+        sender = info.sender,
+        receiver = info.receiver,
+        amount = info.amount,
+    )
+    trans.append(transaction)
+    return transaction
+
